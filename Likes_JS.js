62c2461f000802903b88d19b2f73cb19973f9b59
Francisco Durán <fdduran@uc.cl>
2022-03-25 00:17:07 -0300

    first commit

---
+++ Likes_JS.js
@@ -0,0 +1,27 @@
+
+var count1=1;
+var contarElemento1 = document.querySelector("#count1");
+
+function add1(){
+    count1++;
+    contarElemento1.innerText= count1 +" Like(s)";
+    console.log(count1);
+}
+
+var count2=1;
+var contarElemento2 = document.querySelector("#count2");
+
+function add2(){
+    count2++;
+    contarElemento2.innerText= count2 +" Like(s)";
+    console.log(count2);
+}
+
+var count3=1;
+var contarElemento3 = document.querySelector("#count3");
+
+function add3(){
+    count3++;
+    contarElemento3.innerText= count3 +" Like(s)";
+    console.log(count3);
+}
